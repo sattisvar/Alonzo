af92ac5c38eb1aba289fedc1bef25986c5809801
Sattisvar Tandabany <sattisvar@tandabany.fr>
2017-08-01 23:12:44 +0200

    fix merge

--- src/tools.py
+++ src/tools.py
@@ -1,8 +1,8 @@
 from functools import partial, wraps
 
 def flip(f):
-    'Create a new function from the original one flipping the two first arguments'
-    
+    """Create a new function from the original one flipping the two first arguments
+    """
     def newf(a, b):
         return f(b, a)
     return newf
@@ -12,9 +12,21 @@ def noflip(f):
     return f
 
 def aritylambda(n, f):
-    if n == 1:
+    if n==0:
         return f
-    else:
-        return lambda x: aritylambda(n - 1, partial(f, x))
-                                                  
-    
+    def subvalfun(*x):
+        if len(x) == 0:
+            return f
+        elif len(x) == 1:
+            return aritylambda(n-1, partial(f, x[0]))
+        else:
+            return aritylambda(n-1, partial(f, x[0]))(*x[1:])
+    return subvalfun
+
+def valuefunc(arity):
+    def sbval(*x):
+        if len(x) >= arity:
+            return x[arity-1]
+        else:
+            return valuefunc(arity-len(x))
+    return sbval
